2c5f89fdb50ecc6d2a4dab79e2a0f15cb8e34988
BJ Rutledge <billy.rutledge867037@gmail.com>
2022-07-13 17:39:12 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>BJ Rutledge</h1>
+        <p>Hello! Welcome to BJ Rutledge's resume. I've created this little app to share my resume with Plaid. If you've stumbled on this by happenstance, you can reach me on .</p><a href="www.linkedin.com/bj-rutledge">LinkedIn</a>
+        Have a great day! 
+      </header>
+    </div>
+  );
+}
+
+export default App;
