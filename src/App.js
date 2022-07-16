a7d3a728073cb958ec3df0441011df05070a54c7
BJ Rutledge <billy.rutledge867037@gmail.com>
2022-07-16 07:56:59 -0700

    Fixed external links. Removed create react app boilerplate

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { ExternalLink } from 'react-external-link';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>BJ Rutledge</h1>
-        <p>Hello! Welcome to BJ Rutledge's resume. I've created this little app to share my resume with Plaid. If you've stumbled on this by happenstance, you can reach me on .</p><a href="www.linkedin.com/bj-rutledge">LinkedIn</a>
-        Have a great day! 
+        <p>Hello! Welcome to my resume. I've created this little app to share my resume with Plaid. If you've stumbled on this by happenstance, you can reach me on <ExternalLink href="https://www.linkedin.com/in/bj-rutledge"><span>LinkedIn</span></ExternalLink></p>
+
+        <p>Please see my <a href='BJRutledgeSoftwareEngineerResume.pdf'>resume</a>.</p>
+        <p>The source for this app can be found on <ExternalLink href='https://www.github.com/bj-rutledge'><span>GitHub</span></ExternalLink></p>
+        <h2>Have a great day!</h2> 
       </header>
     </div>
   );
